9eaf61b353870782fbddf5d102f3b3d1fabfc8a2
bruhgit <meowpubgg@gmail.com>
2026-05-07 20:40:07 +0300

    Delete src directory

--- src/compiler.h
+++
@@ -1,8 +0,0 @@
-#ifndef uranium_compiler_h
-#define uranium_compiler_h
-
-#include "value.h"
-
-bool compile(const char* source, FunctionPtr* function);
-
-#endif
--- src/http_native.h
+++
@@ -1,9 +0,0 @@
-#ifndef uranium_http_native_h
-#define uranium_http_native_h
-
-#include "value.h"
-#include <string>
-
-Value nativeHttpRequest(int argCount, const Value* args, std::string* errorMessage);
-
-#endif
